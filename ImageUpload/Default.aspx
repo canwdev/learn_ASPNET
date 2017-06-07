a52d2e0bc26a18739380c69aec18bd226abee5d5
canwdev <googl8812e@gmail.com>
2017-06-07 22:05:41 +0800

    简单图像上传

---
+++ ImageUpload/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="UploadImage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Button ID="btn_upload" runat="server" OnClick="btn_upload_Click" Text="上传图片至服务器的文件系统" />
+        <br />
+        <asp:Image ID="Image1" runat="server" Height="300px" Width="300px" />
+        <br />
+        <asp:Label ID="lbl_image_info" runat="server" Text="..."></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ImageUpload/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class UploadImage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn_upload_Click(object sender, EventArgs e)
+    {
+        string path = Server.MapPath("./upload/");   // 存放路径
+        if (FileUpload1.HasFile)
+        {
+            string fname = FileUpload1.FileName;        // 获取文件名
+            string fext = fname.Substring(fname.LastIndexOf("."));      // 获取文件格式
+
+            if (fext == ".jpg" || fext == ".png" || fext == ".gif")
+            {
+                
+                
+                FileUpload1.SaveAs(path+fname);
+                lbl_image_info.Text = fname + "  ->  " + path + fname + "  OK";
+                Image1.ImageUrl = "./upload/" + fname;
+            }
+            else
+            {
+                Response.Write("<script language=javascript>alert('不支持的图像格式');</" + "script>");
+            }
+        }
+        else
+        {
+            Response.Write("<script language=javascript>alert('null');</" + "script>");
+        }
+        
+    }
+}
